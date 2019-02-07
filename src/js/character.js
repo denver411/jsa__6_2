7db3f49d1760a59a2a13ab20ab0a9904e31ac8f6
Denis Zhebov <denver411@yandex-team.ru>
2019-02-07 20:26:43 +0300

    Выполнена задача №2 к лекции «Прототипы, конструкторы»

--- src/js/character.js
+++ src/js/character.js
@@ -1,5 +1,16 @@
-const fn = (str) => {
+/**
+ *  Создание персонажа
+ *
+ * @param {string} name - Имя персонажа (от 2 до 10 символов)
+ * @param {string} type - Тип персонажа (Bowman, Swordsman, Magician, Daemon, Undead, Zombie)
+ *
+ * @throws Вернется ошибка при неверном типе персонажа
+ * или при имени персонажа короче 2х или длиннее 10 символов
+ */
 
-};
+function Character(name, type) {
+  this.name = name;
+  this.type = type;
+}
 
-export default fn;
+export default Character;
